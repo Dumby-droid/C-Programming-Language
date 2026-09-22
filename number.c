a247ebba05f081241c63020b2da436c70932cd6d
Min Thu Ta <minthuta.mtt9@gmail.com>
2026-09-22 10:12:00 +0000

    Update number program

---
+++ number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int x;
+    int y;
+    scanf("%d %d", &x, &y);
+    printf("Sum: %d\n", x+y);
+    printf("Difference: %d\n", x-y);
+    printf("Product: %d\n", x*y);
+    printf("Quotient: %d\n", x/y);
+    return 0;
+}
